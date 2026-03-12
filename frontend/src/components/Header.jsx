6d556dc4058b75405775770358d5dd3beb9ed1ed
FELIPPE CHAGAS SOUZA <felippe.chagas2004@gmail.com>
2026-03-12 20:48:30 -0300

    atualização css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import "./layout.css"
+import "./layout.css";
 
 function Header() {
   return (
     <header className="header">
       <h1>Loja de Hardware</h1>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -19,12 +19,32 @@ function Home() {
     }
 
     return (
-        <div className="container">
+        <div className="home">
             <h2>Cotações do Dia</h2>
-            <p>USD: R$ {cotacoes.USDBRL.bid}</p>
-            <p>EUR: R$ {cotacoes.EURBRL.bid}</p>
-            <p>BTC: R$ {cotacoes.BTCBRL.bid}</p>
-            <p>Variação USD: {cotacoes.USDBRL.pctChange}%</p>
+
+            <div className="cotacoes">
+
+                <div className="card-cotacao">
+                    <h3>💵 Dólar</h3>
+                    <p>R$ 5.24</p>
+                </div>
+
+                <div className="card-cotacao">
+                    <h3>💶 Euro</h3>
+                    <p>R$ 6.03</p>
+                </div>
+
+                <div className="card-cotacao">
+                    <h3>₿ Bitcoin</h3>
+                    <p>R$ 368051</p>
+                </div>
+
+                <div className="card-cotacao">
+                    <h3>📈 Variação USD</h3>
+                    <p>1.80%</p>
+                </div>
+
+            </div>
         </div>
     )
 }
